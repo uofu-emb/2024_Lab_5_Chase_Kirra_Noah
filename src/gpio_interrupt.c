d8197a18dabb5be99ed1b3d4893b57ae54f3126f
Noah Mecham <nmecham27@gmail.com>
2024-11-11 22:02:41 -0700

    Adding final pictures.

--- src/gpio_interrupt.c
+++ src/gpio_interrupt.c
@@ -8,6 +8,11 @@ void irq_callback(uint gpio, uint32_t event_mask)
 {
     if (gpio != IN_PIN) return;
     toggle = !toggle;
+    volatile int value = 0;
+    while(value < 100000)
+    {
+        value++;
+    }
     if (event_mask & GPIO_IRQ_EDGE_RISE) {
         gpio_put(OUT_PIN, true);
     } else if (event_mask & GPIO_IRQ_EDGE_FALL) {
